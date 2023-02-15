93eb25a887d3d1ef429a15f1ef0790a2ea87538d
Ernest Myrmyr <ernuk@MacBook-Air.local>
2023-02-15 10:39:15 +0200

    just started todo project

---
+++ my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import css from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={css.main_div}>
+      <h1>hello, world!</h1>
+    </div>
+  );
+};
+
+export default App;
